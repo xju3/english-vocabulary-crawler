557e81c03506ae846aad9e866ccbc3fe9aea65af
tju <tju@aeoncloud.cn>
2024-11-01 15:33:40 +0800

    ....

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ sudo apt install build-essential
 sudo apt install libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev
 sudo apt install libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev libffi-dev
 sudo apt install ffmpeg firefox
+sudo add-apt-repository ppa:mozillateam/ppa
 ```
 
 ### 1.2.pyenv
--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from crawler.insta.vocabulary import Vocabulary
 
 
 def main():
-    parameter = sys.argv[1:]
+    parameters = sys.argv[1:]
 
-    if len(parameter) == 0 or parameter[0] == "1":
+    if len(parameters) == 0 or parameters[0] == "1":
         insta_vocabulary = Vocabulary()
         insta_vocabulary.run()
 
-    if len(parameter) > 0:
-        if (parameter[0] == "2"):
+    if len(parameters) > 0:
+        if (parameters[0] == "2"):
             pass
 
-        if (parameter[0] == "3"):
+        if (parameters[0] == "3"):
             pass
 
 
